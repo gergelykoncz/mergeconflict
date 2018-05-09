5ec6cc582ecfb98a712d9a9b40321e2f1403e72d
Gergely Koncz <gergo.koncz@gmail.com>
2018-05-09 08:55:32 +0200

    Generate a conflict

--- bool.js
+++ bool.js
@@ -1,15 +1,15 @@
 var bool = true;
 var boolmasfel = false;
 
-var bool2 = true || (false && true | !false);
+var bool2 = false || (false && true | !false);
 
 var str = 'Sarga bogre gorbe bogre';
 
 var bool3 = (str.length = 3);
 
-console.log(bool3);
+console.log(bool);
 
 var x = 0;
 
-if (x === false) {
+if (x == false) {
 }
